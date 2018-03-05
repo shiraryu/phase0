26d4c5ce0bb3b29a08a3cabfaac929f49848911f
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2018-03-05 14:06:24 +0900

    first commit

---
+++ fetch_rss.rb
@@ -0,0 +1,11 @@
+require "rss"
+url = "https://news.yahoo.co.jp/pickup/computer/rss.xml"
+rss = RSS::Parser.parse(url)
+
+puts rss.channel.title
+puts rss.channel.description
+
+rss.channel.items.each do|item|
+  puts item.title
+  puts item.link
+end
